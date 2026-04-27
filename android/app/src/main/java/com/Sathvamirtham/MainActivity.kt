680b0748f892e0333c7bbac704945d4a4be66de6
exceptionzofficial <exceptionzofficial@gmail.com>
2026-04-27 16:09:48 +0530

    changed package name

--- android/app/src/main/java/com/Sathvamirtham/MainActivity.kt
+++ android/app/src/main/java/com/Sathvamirtham/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.delivery
+package com.Sathvamirtham
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/Sathvamirtham/MainApplication.kt
+++ android/app/src/main/java/com/Sathvamirtham/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.delivery
+package com.Sathvamirtham
 
 import android.app.Application
 import com.facebook.react.PackageList
